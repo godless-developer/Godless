df2437ddc1e950be3171b855f4d1d0caa0925b01
24LP6961 24LP6961 <24LP6961@G304-CC0229.local>
2024-11-21 12:20:36 +0800

    loops

---
+++ Javascript/Function/js.js
@@ -0,0 +1,20 @@
+function functionner() {
+    console.log("working")
+    console.log("Action")
+}
+functionner();
+functionner();
+functionner();
+functionner();
+functionner();
+functionner();
+
+function addnumbers(n1,n2,n3,n4) {
+    console.log(n1 + n2 + n3 + n4)
+}
+addnumbers(2 ,3 ,4 ,5);
+addnumbers(26 ,3 ,42 ,5);
+addnumbers(25 ,25 ,25 ,25);
+
+
+
