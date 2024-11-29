d30f3c0a01415cdcf4fb67ec661f815b48ff1499
24LP6961 24LP6961 <24LP6961@G304-CC0231.local>
2024-11-29 14:04:17 +0800

    bodlogo

--- Javascript/Object/object.js
+++ Javascript/Object/object.js
@@ -9,35 +9,34 @@
 // students.grades[1] = 100;
 
 let students = [
-    { name: "dorjoo", age: 4,gender: "male"},
-    { name: "dulmaa", age: 34,gender: "female"},
-    { name: "dondog", age: 54,gender: "male"},
-    { name: "bataa", age: 2,gender: "male"},
-    { name: "tsetsgee", age: 28,gender: "female"}
+  { name: "dorjoo", age: 4, gender: "male" },
+  { name: "dulmaa", age: 34, gender: "female" },
+  { name: "dondog", age: 54, gender: "male" },
+  { name: "bataa", age: 2, gender: "male" },
+  { name: "tsetsgee", age: 28, gender: "female" },
 ];
 
-
-function findAdults (input) {
-    let adults = [];
-    for (let i = 0;i < input.length; i++) {
-        if (input[i].age >= 18) {
-            adults.push(input[i]);
-        }
+function findAdults(input) {
+  let adults = [];
+  for (let i = 0; i < input.length; i++) {
+    if (input[i].age >= 18) {
+      adults.push(input[i]);
     }
-    return adults;
+  }
+  return adults;
 }
 
 let resultAdults = findAdults(students);
 // console.log(resultAdults);
 
-function findMales (input) {
-    let males = [];
-    for (let i = 0;i < input.length; i++) {
-        if (input[i].gender == "male") {
-            males.push(input[i])
-        }
+function findMales(input) {
+  let males = [];
+  for (let i = 0; i < input.length; i++) {
+    if (input[i].gender == "male") {
+      males.push(input[i]);
     }
-    return males;
+  }
+  return males;
 }
 
 let result = findMales(students);
